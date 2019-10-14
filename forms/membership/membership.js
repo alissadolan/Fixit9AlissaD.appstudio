ae92d6c7da38054b7f12ad202a286a6d983c8958
Alissa Dolan <alissadolan@Alissas-MacBook-Pro-2.local>
2019-10-13 21:24:26 -0500

    Initial commit

---
+++ forms/membership/membership.js
@@ -0,0 +1,13 @@
+var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+btnEnter.onclick=function(){
+
+let firstName = inptFirstName.value
+let membersCheck = members.includes(firstName)
+
+if (membersCheck == true)
+lblMember.value = `${firstName}, you are currently a member.`
+else
+members.push(firstName)
+
+}
